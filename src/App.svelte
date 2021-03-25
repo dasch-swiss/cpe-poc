59e4f6ecbb1b95e034c6b109c08fd7dd64af470e
Eric Sommerhalder <eric.sommerhalder@unibas.ch>
2021-03-25 17:57:22 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import SearchForm from './SearchForm.svelte'
+    import * as json from './test.json'
+</script>
+
+{#each json['SearchForm'] as form}
+    <SearchForm form="{form}"/>
+{/each}
+<style>
+
+</style>
---
+++ src/SearchField.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let value;
+    export let label;
+</script>
+<p>Enter search value for {label}</p>
+<input bind:value={value}>
