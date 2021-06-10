55e4c4a5510fd049496b2a136e455f5cab665f40
EricSommerhalder <38506556+EricSommerhalder@users.noreply.github.com>
2021-06-10 15:41:20 +0200

    Finalized json validator

--- json validator/validate_cpe_json.py
+++ json validator/validate_cpe_json.py
@@ -4,7 +4,6 @@ from jsonschema import validate
 from pprint import pprint
 import tkinter as tk
 from tkinter import filedialog
-import jsonref
 from os.path import dirname
 def validateJson(data, schema):
     try:
@@ -20,9 +19,7 @@ file_path = filedialog.askopenfilename()
 
 with open("cpe.schema.json") as schema_file:
 
-    base_path = dirname(__file__)
-    base_uri = 'file:///{}//'.format(base_path)
-    schema = jsonref.load(schema_file, base_uri, jsonschema=True)
+    schema = json.load(schema_file)
 
     with open(file_path) as json_file:
         data = json.load(json_file)
